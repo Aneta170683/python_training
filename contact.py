8607ad1a9c9d4de7c46a855840192693e6646649
Aneta Serafin <aneta.serafin06@wp.pl>
2018-01-25 22:46:17 +0100

    auxiliary class

---
+++ contact.py
@@ -0,0 +1,10 @@
+class Contact:
+
+    def __init__(self, firstname, lastname, title, company, address, mobile, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
