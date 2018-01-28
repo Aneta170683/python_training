3553321fa246cbdf338c52e177c368b98d7668f1
Aneta Serafin <aneta.serafin06@wp.pl>
2018-01-28 16:58:12 +0100

    stworzenie testu na usunięcie grupy, stworzenie ogólnej fikstury; usunięcie applictation2 - drugiego pliku z fiksturą dla testu add contact;

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
+from fixture.session_contact import SessionContactHelper
 
 
 class Application:
@@ -11,6 +13,8 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.session_contact = SessionContactHelper(self)
+        self.contact = ContactHelper(self)
 
 
     def open_home_page(self):
