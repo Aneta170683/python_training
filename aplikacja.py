d2d4b4dc19eacdb7b5d0019ac5b27d5f4216eacf
Aneta Serafin <aneta.serafin06@wp.pl>
2018-01-30 23:36:28 +0100

    modyfikacja testów kontaków poprzez utworzenie fikstury, przeniesienie metod pomocniczych do aplikacji, usunięcie klasy testowej i klasy zniszczenia

--- aplikacja.py
+++ aplikacja.py
@@ -54,5 +54,48 @@ class Aplikacja:
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
+    def wylogowanie(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def stworzenie_kontaktu(self, Kontakt):
+        wd = self.wd
+        self.otwarcie_strony_z_kontaktami()
+        # stworzenie kontaktu
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(Kontakt.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(Kontakt.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(Kontakt.address)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(Kontakt.home)
+        # potwierdzenie dodania kontaktu
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def otwarcie_strony_z_kontaktami(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def logowanie(self, username, password):
+        wd = self.wd
+        self.otwarcie_strony_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def otwarcie_strony_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
     def destroy(self):
         self.wd.quit()
--- test_dodanie_kontaktu.py
+++ test_dodanie_kontaktu.py
@@ -1,77 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from kontakt import Kontakt
+from aplikacja import Aplikacja
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_dodanie_kontaktu(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False},
-                            firefox_binary="C:/Program Files/Mozilla Firefox ESR/firefox.exe")
-        self.wd.implicitly_wait(60)
+
+@pytest.fixture
+def ap(request):
+    fixture = Aplikacja()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
     
-    def test_dodanie_kontaktu(self):
-        self.logowanie(username="admin", password="secret")
-        self.stworzenie_kontaktu(Kontakt(firstname="Anna", lastname="Kwiatek", address="Wiejska 2", home="111222111"))
-        self.wylogowanie()
+def test_dodanie_kontaktu(ap):
+    ap.logowanie(username="admin", password="secret")
+    ap.stworzenie_kontaktu(Kontakt(firstname="Anna", lastname="Kwiatek", address="Wiejska 2", home="111222111"))
+    ap.wylogowanie()
 
-    def test_dodanie_pustego_kontaktu(self):
-        self.logowanie(username="admin", password="secret")
-        self.stworzenie_kontaktu(Kontakt(firstname="", lastname="", address="", home=""))
-        self.wylogowanie()
-
-    def wylogowanie(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def stworzenie_kontaktu(self, Kontakt):
-        wd = self.wd
-        self.otwarcie_strony_z_kontaktami()
-        # stworzenie kontaktu
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Kontakt.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Kontakt.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(Kontakt.address)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(Kontakt.home)
-        # potwierdzenie dodania kontaktu
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def otwarcie_strony_z_kontaktami(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def logowanie(self, username, password):
-        wd = self.wd
-        self.otwarcie_strony_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def otwarcie_strony_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_dodanie_pustego_kontaktu(ap):
+    ap.logowanie(username="admin", password="secret")
+    ap.stworzenie_kontaktu(Kontakt(firstname="", lastname="", address="", home=""))
+    ap.wylogowanie()
