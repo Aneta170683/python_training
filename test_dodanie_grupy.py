4d695fd8b5529af262066e96c9a2ede413046ae1
Aneta Serafin <aneta.serafin06@wp.pl>
2018-01-30 23:08:26 +0100

    modyfikacja grupy - zmiana na pytest, utworzenie fikstury w teście grupy, usunięcie klasy testowej, usunięcie funkcji zniszczenia z testów

--- test_dodanie_grupy.py
+++ test_dodanie_grupy.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+
+import pytest
 from grupa import Grupa
 from aplikacja import Aplikacja
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_dodanie_grupy(unittest.TestCase):
-    def setUp(self):
-        self.ap = Aplikacja()
-    
-    def test_dodanie_grupy(self):
-        self.ap.logowanie(username="admin", password="secret")
-        self.ap.stworzenie_grupy(Grupa(name="testowanie", header="testy1", footer="testy2"))
-        self.ap.wylogowanie()
+@pytest.fixture
+def ap(request):
+    fixture = Aplikacja()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_dodanie_grupy(ap):
+    ap.logowanie(username="admin", password="secret")
+    ap.stworzenie_grupy(Grupa(name="testowanie", header="testy1", footer="testy2"))
+    ap.wylogowanie()
 
 
-    def test_dodanie_pustej_grupy(self):
-        self.ap.logowanie(username="admin", password="secret")
-        self.ap.stworzenie_grupy(Grupa(name="", header="", footer=""))
-        self.ap.wylogowanie()
+def test_dodanie_pustej_grupy(ap):
+    ap.logowanie(username="admin", password="secret")
+    ap.stworzenie_grupy(Grupa(name="", header="", footer=""))
+    ap.wylogowanie()
 
-
-
-    def tearDown(self):
-        self.ap.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
