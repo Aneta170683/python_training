4932d8251f3e002557003d9455de6b35e22e35f7
Aneta Serafin <aneta.serafin06@wp.pl>
2018-01-30 22:03:28 +0100

    stworzenie testu na dodanie kontaktu, stworzenie metody pomocniczej

---
+++ kontakt.py
@@ -0,0 +1,7 @@
+class Kontakt:
+
+    def __init__(self, firstname, lastname, address, home):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.home = home
--- test_dodanie_kontaktu.py
+++ test_dodanie_kontaktu.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from kontakt import Kontakt
 
 def is_alert_present(wd):
     try:
@@ -12,46 +12,65 @@ def is_alert_present(wd):
 
 class test_dodanie_kontaktu(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False},
+                            firefox_binary="C:/Program Files/Mozilla Firefox ESR/firefox.exe")
         self.wd.implicitly_wait(60)
     
-    def test_test_dodanie_kontaktu(self):
-        success = True
+    def test_dodanie_kontaktu(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("add new").click()
+
+        self.otwarcie_strony_home_page(wd)
+        self.logowanie(wd, username="admin", password="secret")
+        self.otwarcie_strony_z_kontaktami(wd)
+        self.stworzenie_kontaktu(wd, Kontakt(firstname="Anna", lastname="Kwiatek", address="Wiejska 2", home="111222111"))
+        self.wylogowanie(wd)
+
+    def test_dodanie_pustego_kontaktu(self):
+        wd = self.wd
+
+        self.otwarcie_strony_home_page(wd)
+        self.logowanie(wd, username="admin", password="secret")
+        self.otwarcie_strony_z_kontaktami(wd)
+        self.stworzenie_kontaktu(wd, Kontakt(firstname="", lastname="", address="", home=""))
+        self.wylogowanie(wd)
+
+    def wylogowanie(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def stworzenie_kontaktu(self, wd, Kontakt):
+        # stworzenie kontaktu
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Anna")
+        wd.find_element_by_name("firstname").send_keys(Kontakt.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Kwiatek")
+        wd.find_element_by_name("lastname").send_keys(Kontakt.lastname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Wiejska 2")
+        wd.find_element_by_name("address").send_keys(Kontakt.address)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("111222111")
+        wd.find_element_by_name("home").send_keys(Kontakt.home)
+        # potwierdzenie dodania kontaktu
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("Logout").click()
+
+    def otwarcie_strony_z_kontaktami(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def logowanie(self, wd, username, password):
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        self.assertTrue(success)
-    
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def otwarcie_strony_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
 
