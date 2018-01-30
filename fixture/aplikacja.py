3fbb0bde061fc84dfacd30fc1d7af6a30d916ce1
Aneta Serafin <aneta.serafin06@wp.pl>
2018-01-31 00:42:28 +0100

    wyodrębnienie klasy pomocniczej Kontakty - przeniesienie mteod tworzenia kontaktów, usunięcie tych metod z metody aplikacja dla kontaktów

--- fixture/aplikacja.py
+++ fixture/aplikacja.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.sesja import SesjaPomoc
 from fixture.grupa import GrupaPomoc
+from fixture.kontakty import KontaktPomoc
 
 class Aplikacja:
 
@@ -10,40 +11,11 @@ class Aplikacja:
         self.wd.implicitly_wait(60)
         self.sesja = SesjaPomoc(self)
         self.grupa = GrupaPomoc(self)
-
-
-    def otwarcie_strony_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-
-
-    def stworzenie_kontaktu(self, Kontakt):
-        wd = self.wd
-        self.otwarcie_strony_z_kontaktami()
-        # stworzenie kontaktu
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Kontakt.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Kontakt.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(Kontakt.address)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(Kontakt.home)
-        # potwierdzenie dodania kontaktu
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def otwarcie_strony_z_kontaktami(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
+        self.kontakty = KontaktPomoc(self)
 
     def otwarcie_strony_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+
     def destroy(self):
         self.wd.quit()
--- test/test_dodanie_kontaktu.py
+++ test/test_dodanie_kontaktu.py
@@ -13,11 +13,11 @@ def ap(request):
 
     
 def test_dodanie_kontaktu(ap):
-    ap.logowanie(username="admin", password="secret")
-    ap.stworzenie_kontaktu(Kontakt(firstname="Anna", lastname="Kwiatek", address="Wiejska 2", home="111222111"))
-    ap.wylogowanie()
+    ap.sesja.logowanie(username="admin", password="secret")
+    ap.kontakty.stworzenie(Kontakt(firstname="Anna", lastname="Kwiatek", address="Wiejska 2", home="111222111"))
+    ap.sesja.wylogowanie()
 
 def test_dodanie_pustego_kontaktu(ap):
-    ap.logowanie(username="admin", password="secret")
-    ap.stworzenie_kontaktu(Kontakt(firstname="", lastname="", address="", home=""))
-    ap.wylogowanie()
+    ap.sesja.logowanie(username="admin", password="secret")
+    ap.kontakty.stworzenie(Kontakt(firstname="", lastname="", address="", home=""))
+    ap.sesja.wylogowanie()
