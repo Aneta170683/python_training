a57d3a245bd2132129cc73d5e77047a5bb2ca441
Aneta Serafin <aneta.serafin06@wp.pl>
2018-01-31 20:57:14 +0100

    modyfikacja testu dodania kontaktu poprzez dodanie metody logowania z pliku session, modyfikacja pliku application poprzez usunięcie oddzielnej metody do logowania stworzonej do kontaktów

--- fixture/application.py
+++ fixture/application.py
@@ -2,7 +2,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
-from fixture.session_contact import SessionContactHelper
+
 
 
 class Application:
@@ -13,7 +13,6 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-        self.session_contact = SessionContactHelper(self)
         self.contact = ContactHelper(self)
 
 
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,12 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session_contact.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Anna", lastname="Test", title="kontakt", company="Testowa", address="Polna 1", mobile="111222333", email="test1@wp.pl"))
-    app.session_contact.logout()
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.session_contact.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", title="", company="", address="", mobile="", email=""))
-    app.session_contact.logout()
+    app.session.logout()
 
