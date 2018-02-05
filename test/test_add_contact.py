97c344ecd86f50dcf9895fece4fbffc5c0e8852c
Aneta Serafin <aneta.serafin06@wp.pl>
2018-02-05 15:04:08 +0100

    stworzenie nowego testu na modyfikację kontaków

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.contact.create(Contact(firstname="Anna", lastname="Test", title="kontakt", company="Testowa", address="Polna 1", mobile="111222333", email="test1@wp.pl"))
+    app.contact.create(Contact(firstname="Anna", lastname="Test", title="kontakt", company="Testowa", address="Polna 1",
+                               mobile="111222333", email="test1@wp.pl"))
+
 
 def test_add_empty_contact(app):
     app.contact.create(Contact(firstname="", lastname="", title="", company="", address="", mobile="", email=""))
---
+++ test/test_modify_contact.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+def test_modify_contact_firstname(app):
+    app.contact.modify_first_contact(Contact(firstname="New contact"))
+
+
+def test_modify_contact_lastname(app):
+    app.contact.modify_first_contact(Contact(lastname="New lastname"))
