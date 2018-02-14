de216af16873a23bf70ca3ae2b29aa2824caa9bc
Aneta Serafin <aneta.serafin06@wp.pl>
2018-02-14 22:01:12 +0100

    dopracowanie testów - usuwania i modyfikacji dowolnych kontaktów

--- fixture/contact.py
+++ fixture/contact.py
@@ -28,10 +28,17 @@ class ContactHelper:
         # submit modification
         wd.find_element_by_name("update").click()
 
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.home_page()
-        self.select_first_contact()
+        self.select_contact_by_index(index)
         #submit deletion
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
@@ -77,10 +84,13 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
 
-    def modify_first_contact(self, new_contact_data):
+    def modify_first_contact(self):
+        self.modify_contact_by_index(0)
+
+    def modify_contact_by_index(self, index, new_contact_data):
         wd = self.app.wd
         self.home_page()
-        self.select_first_contact()
+        self.select_contact_by_index(index)
         # open modification form
         wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
         # fill contact form
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,11 +1,15 @@
 from model.contact import Contact
+from random import randrange
+
 
 
 def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Aneta", lastname="Kwiatek"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname="Antonina"))
+    index = randrange(len(old_contacts))
+
+    app.contact.modify_contact_by_index(index, Contact(firstname="Antonina"))
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
 
