469afb573b79e2b00f2b4b4ebefd57d9ee0f6c35
Aneta Serafin <aneta.serafin06@wp.pl>
2018-02-20 22:01:30 +0100

    utworzony nowy test test_phones, cięcie łańcuchów na części

--- model/contact.py
+++ model/contact.py
@@ -1,15 +1,36 @@
+from sys import maxsize
+
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, title=None, company=None, address=None, mobile=None, email=None,
-                 id=None):
+    def __init__(self, firstname=None, lastname=None, id=None, title=None, company=None, address=None, homephone= None,
+                 mobilephone=None, workphone=None, secondaryphone = None, all_phones_from_home_page = None,  email=None):
         self.firstname = firstname
         self.lastname = lastname
         self.title = title
         self.company = company
         self.address = address
-        self.mobile = mobile
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.secondaryphone = secondaryphone
+        self.all_phones_from_home_page = all_phones_from_home_page
         self.email = email
         self.id = id
+
+
+    def __repr__(self):
+        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id)\
+                and self.firstname == other.firstname and self.lastname == other.lastname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
+
 
 
 
