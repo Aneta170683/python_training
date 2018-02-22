6e8a09528a6538320bcc3a041051ebe944aef841
Aneta Serafin <aneta.serafin06@wp.pl>
2018-02-22 21:29:45 +0100

    zadanie numer 16

--- test/test_phones.py
+++ test/test_phones.py
@@ -3,8 +3,12 @@ import re
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    # do testu z użyciem weryfikacji wstecznej
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    #do testu bez weryfikacji wstecznej czyli normalna asercja - porównuję to co jest na stronie głównej kontaktów
+    # z tym co jest w polu edycji - musze dodać do class jeśli nie mam lastname, firstname, address,
+    # musze dodąć w metodzie get contact list  oraz w metodzie get contact info from edit page
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address
@@ -26,14 +30,14 @@ def test_phones_on_contact_view_page(app):
 def clear(s):
     return re.sub("[() -]", "", s)
 
-
+#weryfikacja wsteczna z użyciem lambda
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.homephone,  contact.mobilephone, contact.workphone, contact.secondaryphone]))))
 
-
+#weryfikacja wsteczna z użyciem lambda
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
