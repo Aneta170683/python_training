f07d35a99112eb1096e9a46fef7f815c971b4c34
Aneta Serafin <aneta.serafin06@wp.pl>
2018-02-24 19:09:10 +0100

    zadanie numer 16

--- test/test_phones.py
+++ test/test_phones.py
@@ -14,10 +14,6 @@ def test_phones_on_home_page(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
 
 
-
-
-
-
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
