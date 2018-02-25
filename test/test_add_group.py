addd5ba493503541d58ce8a885a623054c19efbe
Aneta Serafin <aneta.serafin06@wp.pl>
2018-02-25 10:49:04 +0100

    oddzielnie testowych danych od skryptów - utworzenie paczki data a w niej pliku app_group.py, generacja testowych danych w formacie json - utworzenie paczki generator a w niej group.py

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,30 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from sys import maxsize
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-#jedna grupa z pustymi danym i kilka grup z niepustymi danymi przypadkowymi
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
-]
-
-#kombinacja w dodaniu grup tzn. np. pusty name, uzupełnione footer, uzupełnione header albo pusten name,
-# uzupełnione footer, puste header
-
-#testdata = [
-    #Group(name=name, header=header, footer=footer)
-    #for name in ["", random_string("name", 10)]
-    #for header in ["", random_string("header", 20)]
-    #for footer in ["", random_string("footer", 20)]
-#]
-
+from data.add_group import constant as testdata
 
 
 
@@ -46,6 +23,17 @@ def test_add_group(app, group):
     #assert len(old_groups) + 1 == len(new_groups)
     #old_groups.append(group)
     #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+# kombinacja w dodaniu grup tzn. np. pusty name, uzupełnione footer, uzupełnione header albo pusten name,
+# uzupełnione footer, puste header
+
+# testdata = [
+# Group(name=name, header=header, footer=footer)
+# for name in ["", random_string("name", 10)]
+# for header in ["", random_string("header", 20)]
+# for footer in ["", random_string("footer", 20)]
+# ]
 
 
 
