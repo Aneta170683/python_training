483b076dc9232080de2f1fdcfe301144ca0ff3cb
Aneta Serafin <aneta.serafin06@wp.pl>
2018-02-25 22:41:49 +0100

    zmiana nazwy pliku z add_contact.py na contacts.py

---
+++ data/contacts.py
@@ -0,0 +1,15 @@
+
+from model.contact import Contact
+import random
+import string
+
+
+testdata = [
+    Contact(firstname="firstname1", lastname="lastname1",
+            title="title1", company="company1", address="addres1", mobilephone="mobilephone1", email="email1"),
+    Contact(firstname="firstname2", lastname="lastname2",
+            title="title2", company="company2", address="addres2", mobilephone="mobilephone2", email="email2")
+]
+
+
+
