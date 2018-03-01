f3fbb7b434ae347dad49fa511b75513a0b232670
Aneta Serafin <aneta.serafin06@wp.pl>
2018-03-01 20:35:07 +0100

    Testy dodania i usunięcia grupy zmodyfikowane tak aby dane ładowały sie z bazy danych a nie interfejsu użytkownika

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
-def test_add_group(app, json_groups):
+# test z bazy danych
+def test_add_group(app, db, json_groups):
         group = json_groups
-        old_groups = app.group.get_group_list()
-        #group = Group(name="test3", header="test3", footer="test3")
+        old_groups = db.get_group_list()
+        # group = Group(name="test3", header="test3", footer="test3")
         app.group.create(group)
-        assert len(old_groups) + 1 == app.group.count()
-        new_groups = app.group.get_group_list()
+        new_groups = db.get_group_list()
         old_groups.append(group)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+# test z interfejsu użytkownika
+#def test_add_group(app, json_groups):
+        #group = json_groups
+        #old_groups = app.group.get_group_list()
+                #group = Group(name="test3", header="test3", footer="test3")
+        #app.group.create(group)
+        #assert len(old_groups) + 1 == app.group.count()
+        #new_groups = app.group.get_group_list()
+        #old_groups.append(group)
+        #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 #def test_add_empty_group(app):
     #old_groups = app.group.get_group_list()
