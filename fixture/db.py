fb8cd4f6f6d498cf89271cc803a370540da5ee88
Aneta Serafin <aneta.serafin06@wp.pl>
2018-03-01 22:29:52 +0100

    problem z odłączanym sprawdzaniem interfejsu użytkownika - dla usuwania grup

--- fixture/db.py
+++ fixture/db.py
@@ -13,6 +13,7 @@ class DbFixture:
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
 
 
+
     def get_group_list(self):
         list = []
         cursor = self.connection.cursor()
