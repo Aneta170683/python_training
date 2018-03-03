c8974352b698de5a766c33be030181c67f8df3f3
Aneta Serafin <aneta.serafin06@wp.pl>
2018-03-03 22:33:15 +0100

    odłączane sprawdzanie interfejsu użytkownika - modyfikacja grup

--- fixture/group.py
+++ fixture/group.py
@@ -60,6 +60,21 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+
+    def modify_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+
     def delete_first_group(self):
         self.delete_group_by_index(0)
 
@@ -90,6 +105,13 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
 
     def fill_group_form(self, group):
         wd = self.app.wd
